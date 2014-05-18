1f85fa1555e0df788e456ea0730ac88d8b2a1a72
jesusdario <jesusdriru@gmail.com>
2014-05-18 13:56:30 +0200

    super formulote

--- chaos.js
+++ chaos.js
@@ -21,28 +21,17 @@ function Chaos() {
 	try {
 		context = new (window.AudioContext || window.webkitAudioContext);
 
-		/* Nodes creation and routing */
+		/* Nodes creation and setup */
 		/* Node creation los operadores terciarios son para compatibilidad */
 		delay 	   = context.createDelay ? context.createDelay() : context.createDelayNode();
 		volume	   = context.createGain  ? context.createGain()  :  context.createGainNode();
 		feedback   = context.createGain  ? context.createGain()  :  context.createGainNode();
 		filter 	   = context.createBiquadFilter();
 		compressor = context.createDynamicsCompressor();
-
-		/* Node conection */
-		// filter.type = 'lowpass';
-		filter.connect( compressor );
-		filter.connect( delay );
-		delay.connect( feedback );
-		feedback.connect( delay );	
-		delay.connect( compressor );
-		compressor.connect( volume );
-		volume.connect( context.destination );
-		
-		//volume.connect( visualizer.analyser );
+		volume.gain.value = 1;
 
 	} catch (e) {
-		alert('No web audio source support in this browser');
+		alert('No web audio source support in this browser' + e);
 	}
 }
 
@@ -62,60 +51,82 @@ Chaos.prototype.calculateGain = function( event ) {
 	// 1. Hay que ajustar las coordenadas.
 	x = event.pageX - this.div.offsetLeft;
 	// 2. ganancia relativa al centro del pad
+	// sigue una exponencial cuadratica creciente con un factor aqui mi calculo
+	// http://www.wolframalpha.com/input/?i=%28x%2F500%29*e^%28+%28x%2F500%29^2+-+1+%29+
 	// Min = 1; max = 2, ancho = 500px
-	var f = x/(500+x);//valores mas o menos arbitrarios
+	if ( x < 50 ) {
+		f = 0;
+	} else {
+		f = Math.pow(x/(500+0.1*x),2)*Math.exp( Math.pow(x/500,2) - 1 );
+	}
+	log( "exp: " + f );
 	//si f es mayor que uno, es un sistema inestable!
 	return f;
 };
 
-Chaos.prototype.shutDown = function( wave ) {
+Chaos.prototype.route = function() {
+	try {
+		/* Node conection */
+		oscillator.connect( filter );
+		//filter.type = 'lowpass';
+		filter.connect( compressor );
+		filter.connect( delay );
+		delay.connect( compressor );
+		delay.connect( feedback );
+		feedback.connect( delay );	
+		compressor.connect( volume );
+		volume.connect( visualizer.analyser );
+		volume.connect( context.destination );
+
+	} catch ( exception ){
+		alert("Unable to set up. " + exception);
+	}
+};
+
+Chaos.prototype.shutdown = function( ) {
 // Funcion molona para apagar guay una onda
 // No funciona muy bien :(
-	var F = Math.floor(wave.frequency.value);
-	for( f = F; f > 1; Math.floor( f = f - f/300 ) ) {
-		//window.setInterval(function(){ 
-			wave.frequency.value = f;
-		//}, 10);
-}
-window.setInterval(	function(){
-	wave.noteOff ? wave.noteOff(0) : wave.stop(0);
-} , 300 );
+	try {
+		var F = Math.floor(oscillator.frequency.value);
+		for( f = F; f > 1; Math.floor( f = f - f/300 ) ) {
+			oscillator.frequency.value = f;
+		}
+		oscillator.noteOff ? oscillator.noteOff(0) : oscillator.stop(0);
+	} catch ( exception ) {
+		alert(" Syntax error. " + exception )
+	}
 };
 
 var chaos = new Chaos();
 var visualizer = new Visualizer( context );
 
-
 chaos.div.onmousedown = function( event ) {
 
 	/* Calculate parameters */
 	oscillator = context.createOscillator ? context.createOscillator() : context.createOscillatorNode ();
-	oscillator.connect( filter );
+	chaos.route();
 	var freq =  chaos.calculateFrequency( event );
 	oscillator.frequency.value = freq;
-    // filter.frequency.value = 2*freq;
-	volume.gain.value = 0.63;
-	feedback.gain.value = chaos.calculateGain( event );
-	/* Begin the magic */
-	oscillator.type = waveforms[iterator];
-	oscillator.noteOn ? oscillator.noteOn(0) : oscillator.start(0);
-	//visualizer.animate();
-	PLAYING = true;
+    feedback.gain.value = chaos.calculateGain( event );
+    oscillator.type = waveforms[iterator];
+    oscillator.noteOn ? oscillator.noteOn(0) : oscillator.start(0);
+    visualizer.animate();
+    PLAYING = true;
 }
 
 chaos.div.onmousemove = function( event ) {
 	if( PLAYING ) {
 		var freq =  chaos.calculateFrequency( event );
 		oscillator.frequency.value = freq;
-		// filter.frequency.value = 2*freq;
 		feedback.gain.value = chaos.calculateGain( event );
 	}
 }
 
 /* Que pare (parar sonido) siempre al quitar un click */
 document.onmouseup = function() {
-	chaos.shutDown(oscillator);
-	//visualizer.clear( );
+	//chaos.shutDown();
+	oscillator.noteOff ? oscillator.noteOff(0) : oscillator.stop(0);
+	visualizer.clear( );
 	PLAYING = false;
 }
 
@@ -129,13 +140,13 @@ waveform_btn.onclick = function(){
 var start = 0;
 var taptap_btn = document.getElementById('tap-tap');
 taptap_btn.onmousedown = function( ) {
-    if( start == 0 ) {
-    	log(start);
-        start = new Date().getTime();
-    } else {
-        var elapsed = new Date().getTime() - start;
-        delay.delayTime.value = elapsed * 0.001;
-        log("Delay: " + elapsed);
+	if( start == 0 ) {
+		log(start);
+		start = new Date().getTime();
+	} else {
+		var elapsed = new Date().getTime() - start;
+		delay.delayTime.value = elapsed * 0.001;
+		log("Delay: " + elapsed);
         // start again
         start = 0;
         return (taptap_btn.innerHTML = elapsed + "ms");
