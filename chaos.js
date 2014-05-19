9252bafd5b023b878a6fb49d49de98b92c2eb741
Jesus <jesusdriru@gmail.com>
2014-05-19 16:15:31 +0200

    Filter perfect balance —finally :-) — !

--- chaos.js
+++ chaos.js
@@ -1,3 +1,4 @@
+/*jslint devel: true, newcap: true, plusplus: true */
 /*
 * Chaos pad by Jesus Rivera
 * Universidad de Sevilla
@@ -74,8 +75,19 @@ Chaos.prototype.calculateFrequency = function (event) {
 
 Chaos.prototype.setFilterFrequency = function (event) {
     'use strict';
-    var f = oscillator.frequency.value;
-    filter.frequency.value = 5 * f;
+    var y, min, max, octave_number, alpha;
+    y = event.pageY - this.div.offsetTop;
+    min = 22; // min 40Hz
+    max = context.sampleRate / 2; // max half of the sampling rate
+    // Logarithm (base 2) to compute how many octaves fall in the range.
+    octave_number = Math.log(max / min) / Math.LN2;
+    // Compute a parameter from 0 to 1 based on an exponential scale.
+    alpha = Math.pow(2, octave_number * (((2 / this.div.clientHeight) * (this.div.clientHeight - y)) - 1.0));
+    if (filter.type == 'highpass' || filter.type == 'bandpass') {
+        filter.frequency.value = min / alpha;
+    } else {
+        filter.frequency.value = max * alpha;
+    }
 };
 
 Chaos.prototype.calculateGain = function (event) {
@@ -109,44 +121,49 @@ chaos.div.onmousedown = function (event) {
         chaos.setFilterFrequency(event);
         chaos.calculateGain(event);
         oscillator.type = waveforms[current_waveform];
-        oscillator.noteOn ? oscillator.noteOn(0) : oscillator.start(0);
         PLAYING = true;
-    } catch (exception){
+        return oscillator.noteOn ? oscillator.noteOn(0) : oscillator.start(0);
+    } catch (exception) {
         alert(exception);
     }
-}
+};
 
-chaos.div.onmousemove = function( event ) {
-    if( PLAYING ) {
-        chaos.calculateFrequency( event );
-        chaos.setFilterFrequency( event );
-        chaos.calculateGain( event );
+chaos.div.onmousemove = function (event) {
+    'use strict';
+    if (PLAYING) {
+        chaos.calculateFrequency(event);
+        chaos.setFilterFrequency(event);
+        chaos.calculateGain(event);
     }
-}
+};
 
 /* Que pare (parar sonido) siempre al quitar un click */
-document.onmouseup = function() {
-    oscillator.noteOff ? oscillator.noteOff(0) : oscillator.stop(0);
+document.onmouseup = function () {
+    'use strict';
     PLAYING = false;
-}
+    return oscillator.noteOff ? oscillator.noteOff(0) : oscillator.stop(0);
+};
 
 var waveform_btn = document.getElementById('waveform');
-waveform_btn.onclick = function(){
-    current_waveform = (current_waveform < 3) ? (current_waveform+1) : 0;
+waveform_btn.onclick = function () {
+    'use strict';
+    current_waveform = (current_waveform < 3) ? (current_waveform + 1) : 0;
     return (waveform_btn.innerHTML = waveforms[current_waveform]);
 };
 
 var filter_btn = document.getElementById('filter');
-filter_btn.onclick = function(){
-    current_filter = (current_filter < 3) ? (current_filter+1) : 0;
+filter_btn.onclick = function () {
+    'use strict';
+    current_filter = (current_filter < filters.length) ? (current_filter + 1) : 0;
     filter.type = current_filter;
     return (filter_btn.innerHTML = filters[current_filter]);
 };
 
 var start = 0;
 var taptap_btn = document.getElementById('tap-tap');
-taptap_btn.onmousedown = function( ) {
-    if( start == 0 ) {
+taptap_btn.onmousedown = function () {
+    'use strict';
+    if (start === 0) {
         start = new Date().getTime();
     } else {
         var elapsed = new Date().getTime() - start;
